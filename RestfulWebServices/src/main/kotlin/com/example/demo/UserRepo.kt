911db006926dfbccfc1947d5dcbe23845f222015
akshaysyaduvanshi <akshaysyaduvanshi@gmail.com>
2018-08-29 00:13:09 +0530

    Adding the constraints information to the swagger UI

--- RestfulWebServices/src/main/kotlin/com/example/demo/UserRepo.kt
+++ RestfulWebServices/src/main/kotlin/com/example/demo/UserRepo.kt
@@ -1,5 +1,7 @@
 package com.example.demo
 
+import io.swagger.annotations.ApiModel
+import io.swagger.annotations.ApiModelProperty
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 import javax.persistence.Entity
@@ -8,12 +10,14 @@ import javax.persistence.Id
 import javax.validation.constraints.Size
 
 @Entity
+@ApiModel
 class User {
     @Id
     @GeneratedValue
     var id: Int = 0
 
     @Size(min = 2, message = "Name should be at lease two chars")
+    @ApiModelProperty("Name should be at lease two chars")
     var name: String = ""
     var location: String = ""
 }
