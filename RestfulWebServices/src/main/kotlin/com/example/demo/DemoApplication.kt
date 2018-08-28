ad495f789cf6e3e61f0ec70b035db9214f32fff9
akshaysyaduvanshi <akshaysyaduvanshi@gmail.com>
2018-08-28 23:54:24 +0530

    Added swagger ui

--- RestfulWebServices/src/main/kotlin/com/example/demo/DemoApplication.kt
+++ RestfulWebServices/src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -3,8 +3,12 @@ package com.example.demo
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.LocaleResolver
 import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver
+import springfox.documentation.spi.DocumentationType
+import springfox.documentation.spring.web.plugins.Docket
+import springfox.documentation.swagger2.annotations.EnableSwagger2
 import java.util.*
 
 
@@ -15,6 +19,15 @@ class DemoApplication {
         val resolver = AcceptHeaderLocaleResolver()
         resolver.defaultLocale = Locale.US
         return resolver
+    }
+}
+
+@Configuration
+@EnableSwagger2
+class SwaggerConfig {
+    @Bean
+    fun api(): Docket {
+        return Docket(DocumentationType.SWAGGER_2)
     }
 }
 
