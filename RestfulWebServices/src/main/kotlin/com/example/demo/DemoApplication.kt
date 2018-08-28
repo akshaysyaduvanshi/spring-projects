dcc80cbef05409a474d6fe723e6b2820e7aaddb2
akshaysyaduvanshi <akshaysyaduvanshi@gmail.com>
2018-08-28 23:15:06 +0530

    Added internatinalization code snippet

--- RestfulWebServices/src/main/kotlin/com/example/demo/DemoApplication.kt
+++ RestfulWebServices/src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -11,7 +11,19 @@ import java.util.*
 
 @SpringBootApplication
 class DemoApplication {
-    
+    @Bean
+    fun localeResolver(): LocaleResolver {
+        val resolver = SessionLocaleResolver()
+        resolver.setDefaultLocale(Locale.US)
+        return resolver
+    }
+
+    @Bean
+    fun bundleResourceSource(): ResourceBundleMessageSource {
+        val bundleResourceSource = ResourceBundleMessageSource()
+        bundleResourceSource.setBasename("messages")
+        return bundleResourceSource
+    }
 }
 
 fun main(args: Array<String>) {
